3d6bc5b1425a3982a11705dc3e172f56e949db6c
Sacha <sacha.harel@gmail.com>
2024-02-27 10:35:00 +0100

    update page.tsx and header.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div>
       <Header />
+
+      <h1>
+        Bienvenue sur Exploitation, une application de gestion de modules pour l&aposESIEE-IT
+      </h1>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <div>
-            <header className="bg-black text-white flex">
+            <header className="bg-black text-white flex h-20 items-center justify-around">
                 <h1>Exploitation</h1>
-                <ul className='flex absolute right-0'>
+                <ul className='flex'>
                     <li>
                         <a href="/modules" className=''>Vos modules</a>
                     </li>
                     <li>
-                        <a href="/login" className='ml-10'>Se connecter</a>
+                        <a href="/login" className='ml-20'>Se connecter</a>
                     </li>
                 </ul>
             </header>
