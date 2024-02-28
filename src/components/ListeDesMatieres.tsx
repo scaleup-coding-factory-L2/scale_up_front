81ba7f4224a4900e1435468a4bcc8650276e6e0e
rat-balls <delalande.ethan@gmail.com>
2024-02-28 11:13:33 +0100

    feat: made the form scale with number of subjects

--- src/components/ListeDesMatieres.tsx
+++ src/components/ListeDesMatieres.tsx
@@ -1,37 +1,43 @@
 'use client';
 
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 export default function ListeDesMatieres() {
-    const [ptfFileName, setPtfFileName] = useState("");
     const [syllabusFileName, setSyllabusFileName] = useState("");
-
-    const ptfRef = useRef<HTMLInputElement>(null);
-    const syllabusRef = useRef<HTMLInputElement>(null);
     
-    function handleChange() {
-        if(ptfRef.current != null && ptfRef.current.files != null && ptfRef.current.files[0] != null){
-            setPtfFileName(ptfRef.current.files[0].name)
-        }
-        if(syllabusRef.current != null && syllabusRef.current.files != null && syllabusRef.current.files[0] != null){
-            setSyllabusFileName(syllabusRef.current?.files[0].name)
+    function handleChange(e: React.FormEvent<HTMLInputElement>) {
+        if(e.currentTarget != null && e.currentTarget.files != null && e.currentTarget.files[0] != null){
+            setSyllabusFileName(e.currentTarget?.files[0].name)
         }
       }
 
+    const matières = [{
+        name: 'Matiere1'
+    },
+    {
+        name: 'Matiere2'
+    },
+    {
+        name: 'Matiere3'
+    }]
+
     return(
         <>
-        <form className="flex bg-slate-200 p-4 rounded-md items-center" action="http://localhost:3000/api/uploadPDF" method="post" encType="multipart/form-data">
-            <p className="p-2 pr-16">Matière 1</p>
-            <label htmlFor="ptfBtn" className="bg-slate-300 mr-4 p-2 hover:bg-slate-400 active:bg-slate-500 rounded-md">{ptfFileName == "" ?
-                "📎 upload PTF" : ptfFileName
-            }</label>
-            <label htmlFor="syllabusBtn" className="bg-slate-300 mr-4 p-2 hover:bg-slate-400 active:bg-slate-500 rounded-md">{syllabusFileName == "" ?
-                "📎 upload syllabus" : syllabusFileName
-            }</label>
-            <input ref={ptfRef} id="ptfBtn" className="hidden" type="file" name="ptf" onChange={handleChange}/>
-            <input ref={syllabusRef} id="syllabusBtn" className="hidden" type="file" name="syllabus" onChange={handleChange}/>
-            <button type="submit" className="bg-slate-400 hover:bg-slate-500 active:bg-slate-600 p-3 rounded-md">Upload</button>
-        </form>
+            <div className="bg-slate-50 shadow-md p-6 w-1/3 h-full mx-2 rounded-md">
+                <p>Matières du contrat :</p>
+                {matières.map((matiere, index) => {
+                        return(
+                            <form key={index} className="flex bg-slate-200 h-full rounded-md my-2 items-center" action="http://localhost:3000/api/uploadSyllabus" method="post" encType="multipart/form-data">
+                                <p className="w-1/2 m-2">{matiere.name}</p>
+                                <label htmlFor="syllabusBtn" className="bg-slate-300 hover:bg-slate-400 m-2 active:bg-slate-500 w-1/4 rounded-md">{syllabusFileName == "" ?
+                                    "📎 upload syllabus" : syllabusFileName
+                                }</label>
+                                <input id="syllabusBtn" className="hidden" type="file" name="syllabus" onChange={handleChange}/>
+                                <button type="submit" className="bg-slate-400 hover:bg-slate-500 m-2 active:bg-slate-600 w-1/4 rounded-md">Upload</button>
+                            </form>
+                        )
+                    })}
+            </div>
         </>
     )
 }
