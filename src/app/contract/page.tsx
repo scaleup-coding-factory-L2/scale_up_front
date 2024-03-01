5847c74960af56ca3ebc3bddc18944e69aa7e90d
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-01 14:53:14 +0100

    feat: Refactor form inputs and add link to contract page

--- src/app/contract/page.tsx
+++ src/app/contract/page.tsx
@@ -1,18 +1,42 @@
-import React from "react";
-
-export const page = () => {
+export default function page() {
   return (
     <div>
       page
       <form action="">
         <label htmlFor="hourlyPrice"></label>
-        <input type="number" name="hourlyPrice" id="hourlyPrice" />
+        <input
+          type="number"
+          name="hourlyPrice"
+          id="hourlyPrice"
+          required
+          placeholder="hourlyPrice"
+        />
         <label htmlFor="hoursVolume"></label>
-        <input type="" name="hoursVolume" id="hoursVolume" />
-        <label htmlFor=""></label>
-        <input type="text" name="password" id="password" />
-        <input type="submit" value="submit" />
+        <input
+          type="number"
+          name="hoursVolume"
+          id="hoursVolume"
+          required
+          placeholder="hoursVolume"
+        />
+        <label htmlFor="startDate"></label>
+        <input
+          type="date"
+          name="startDate"
+          id="startDate"
+          required
+          placeholder="startDate"
+        />
+        <label htmlFor="endDate"></label>
+        <input
+          type="date"
+          name="endDate"
+          id="endDate"
+          required
+          placeholder="endDate"
+        />
+        <input type="submit" value="submit" required />
       </form>
     </div>
   );
-};
+}
