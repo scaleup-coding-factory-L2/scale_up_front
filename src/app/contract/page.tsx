fd404c05cd90f5bfacc900e63a42a1e718c1b0e8
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-01 11:48:14 +0100

    feat: start form contract

---
+++ src/app/contract/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const page = () => {
+  return (
+    <div>
+      page
+      <form action="">
+        <label htmlFor="hourlyPrice"></label>
+        <input type="number" name="hourlyPrice" id="hourlyPrice" />
+        <label htmlFor="hoursVolume"></label>
+        <input type="" name="hoursVolume" id="hoursVolume" />
+        <label htmlFor=""></label>
+        <input type="text" name="password" id="password" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+};
