83cd86b6ec8a71b69d434635543441d1c59315f5
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-04 19:17:31 +0100

    feature: Add form validation and error handling to contract page

--- src/app/contract/page.tsx
+++ src/app/contract/page.tsx
@@ -1,41 +1,98 @@
-export default function page() {
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const contractFormSchema = z.object({
+  hourlyPrice: z.number().positive(),
+  hoursVolume: z.number().positive(),
+  startDate: z.date(),
+  endDate: z.date(),
+});
+
+type contractFormField = z.infer<typeof contractFormSchema>;
+
+export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<contractFormField>({ resolver: zodResolver(contractFormSchema) });
+
+  const onSubmit = async (data: contractFormField) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      console.log(data);
+    } catch (error) {
+      setError("root", {
+        message: "Une erreur est survenue lors de la génération du contrat",
+      });
+    }
+  };
+
   return (
     <div>
-      page
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="hourlyPrice"></label>
         <input
+          {...register("hourlyPrice")}
           type="number"
           name="hourlyPrice"
           id="hourlyPrice"
-          required
           placeholder="hourlyPrice"
         />
+
+        {errors.hourlyPrice && (
+          <div className="text-red-700">{errors.hourlyPrice.message}</div>
+        )}
+
         <label htmlFor="hoursVolume"></label>
         <input
+          {...register("hoursVolume")}
           type="number"
           name="hoursVolume"
           id="hoursVolume"
-          required
           placeholder="hoursVolume"
         />
+
+        {errors.hoursVolume && (
+          <div className="text-red-700">{errors.hoursVolume.message}</div>
+        )}
+
         <label htmlFor="startDate"></label>
         <input
+          {...register("startDate")}
           type="date"
           name="startDate"
           id="startDate"
-          required
           placeholder="startDate"
         />
+
+        {errors.startDate && (
+          <div className="text-red-700">{errors.startDate.message}</div>
+        )}
+
         <label htmlFor="endDate"></label>
         <input
+          {...register("endDate")}
           type="date"
           name="endDate"
           id="endDate"
-          required
           placeholder="endDate"
         />
-        <input type="submit" value="submit" required />
+
+        {errors.endDate && (
+          <div className="text-red-700">{errors.endDate.message}</div>
+        )}
+
+        <button disabled={isSubmitting} type="submit">
+          {isSubmitting ? "En cours de génération" : "Générer le contat"}
+        </button>
+
+        {errors.root && (
+          <div className="text-red-700">{errors.root.message}</div>
+        )}
       </form>
     </div>
   );
